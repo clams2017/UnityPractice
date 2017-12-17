3f2a12656de5339ff58611457ec3dd480ca4123c
umamo <32873449+umamo@users.noreply.github.com>
2017-12-17 21:37:19 +0900

    [add] 下タブの画面遷移を追加

---
+++ Assets/Scripts/TransitionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionHandler : MonoBehaviour {
+
+	public void toTopScene(){
+		SceneManager.LoadScene ("Top");
+	}
+
+	public void toCompleteListScene(){
+		SceneManager.LoadScene ("CompleteList");
+	}
+
+	public void toBreedingScene(){
+		SceneManager.LoadScene ("Breeding");
+	}
+
+	public void toSettingScene(){
+		SceneManager.LoadScene ("Setting");
+	}
+}
